f612ab0dac916fda4a7969d2e031961d12ea6f3f
StevenPss <pulessteven@gmail.com>
2021-05-26 21:32:31 +0200

    style: added header, navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.scss';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="app">
       <div id="bg" className="h-full w-full absolute"></div>
+      <div className="max-w-6xl mx-auto px-2 sm:px-6 lg:px-8">
+        <Header/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { RiMenu4Fill } from 'react-icons/ri';
+import { BiUser, BiShoppingBag } from 'react-icons/bi';
+
+function Navbar() {
+    return (
+        <nav className="navbar bg-transparent">
+           <div className="relative flex items-center justify-between h-16">
+                    
+                    <div className="flex items-center justify-center sm:items-stretch sm:justify-start">
+                        <div className="flex-shrink-0 flex items-center">
+                            <h1 className="font-black text-2xl navbar-name"><RiMenu4Fill/></h1>
+                        </div>
+                    </div>
+
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                        <div className="block sm:ml-6">
+                            <div className="flex space-x-4 navbar-links">
+                                <a href="#home" className="text-white navbar-icons px-2 py-2 rounded-full text-2xl font-medium" aria-current="page"><BiUser/></a>
+                                <a href="#shop" className="text-white navbar-icons px-2 py-2 rounded-full text-2xl font-medium"><BiShoppingBag/></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </nav>
+    )
+}
+
+export default Navbar
